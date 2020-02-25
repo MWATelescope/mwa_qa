1d1686734c09dae46a5770c73500d6e89cf70f46
Christopher H. Jordan <christopherjordan87@gmail.com>
2020-02-25 11:37:58 +0800

    Use Andrew's fancy "dict" method.

--- mwaqa/metadata.py
+++ mwaqa/metadata.py
@@ -63,7 +63,7 @@ class Query(object):
         u = "{0}&{1}".format("http://mro.mwa128t.org/metadata/find/?search=search",
                              urlencode(self.params))
         if self.extended:
-            u += "&extended"
+            u += "&dict"
         self.url = u
 
     def make_query(self, warn=True):
@@ -77,34 +77,69 @@ class Query(object):
         except URLError as error:
             raise ValueError("URL or network error: %s" % error.reason)
 
-        if self.extended:
-            columns = ("Obsid",
-                       "End time",
-                       "Obs Name",
-                       "Creator",
-                       "ProjectID",
-                       "Right Ascension [deg]",
-                       "Declination [deg]",
-                       "Azimuth [deg]",
-                       "Elevation [deg]",
-                       "Pointing RA [deg]",
-                       "Pointing Dec [deg]",
-                       "LST [deg]",
-                       "Gridpoint")
-        else:
-            columns = ("Obsid",
-                       "Obs Name",
-                       "Creator",
-                       "ProjectID",
-                       "Right Ascension [deg]",
-                       "Declination [deg]")
-
         # Warn if we've hit the pagesize limit of results.
         if warn and len(results) >= self.params["pagesize"]:
             print("Query results may be truncated due to the pagesize parameter.",
                   file=sys.stderr)
 
-        self.table = Table(np.array(results), names=columns)
+        if self.extended:
+            columns = (('local_sidereal_time_deg', "LST [deg]"),
+                       ('mwas.creator', "Creator"),
+                       ('mwas.dataquality', "Data Quality"),
+                       ('mwas.dataqualitycomment', "Data Quality Comment"),
+                       ('mwas.dec_phase_center', "Pointing Dec [deg]"),
+                       ('mwas.freq_res', "Freq. Res. [kHz]"),
+                       ('mwas.int_time', "Int. Time [s]"),
+                       ('mwas.mode', "Mode"),
+                       ('mwas.obsname', "Obs. Name"),
+                       ('mwas.projectid', "ProjectID"),
+                       ('mwas.ra_phase_center', "Pointing RA [deg]"),
+                       ('mwas.starttime', "Obsid"),
+                       ('mwas.stoptime', "Stop Time"),
+                       ('numfiles', "Num. Files"),
+                       ('rfs.frequencies', "Freq. Chans"),
+                       ('sm.azimuth_pointing', "Azimuth [deg]"),
+                       ('sm.dec_pointing', "Dec [deg]"),
+                       ('sm.elevation_pointing', "Elevation [deg]"),
+                       ('sm.gridpoint_number', "Gridpoint"),
+                       ('sm.ra_pointing', "RA [deg]"))
+
+            # astropy doesn't know how to format the "Freq. Chans" column of the
+            # metadata (a list of ints for each frequency of the coarse-band
+            # channels). Just turn it into a string.
+            for r in results:
+                r["rfs.frequencies"] = str(r["rfs.frequencies"])
+            self.table = Table(results)
+            self.table.rename_columns(*list(zip(*columns)))
+            self.table = self.table["Obsid",
+                                    "Stop Time",
+                                    "Creator",
+                                    "ProjectID",
+                                    "Obs. Name",
+                                    "RA [deg]",
+                                    "Dec [deg]",
+                                    "Pointing RA [deg]",
+                                    "Pointing Dec [deg]",
+                                    "Azimuth [deg]",
+                                    "Elevation [deg]",
+                                    "Gridpoint",
+                                    "LST [deg]",
+                                    "Freq. Res. [kHz]",
+                                    "Int. Time [s]",
+                                    "Mode",
+                                    "Num. Files",
+                                    "Freq. Chans",
+                                    "Data Quality",
+                                    "Data Quality Comment",
+                                    ]
+        else:
+            columns = ("Obsid",
+                       "Obs. Name",
+                       "Creator",
+                       "ProjectID",
+                       "RA [deg]",
+                       "Dec [deg]")
+            self.table = Table(np.array(results), names=columns)
 
     def write_csv(self, output_filename):
         ap_ascii.write(self.table,
